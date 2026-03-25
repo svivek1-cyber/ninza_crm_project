c25cf6dcd117ed8d4efc7ae3fb314f337b7822b2
svive <svive@X-thing>
2026-03-25 16:51:18 +0530

    modified base class

--- ninza_crm_project/src/main/java/generic_utility/BaseClass.java
+++ ninza_crm_project/src/main/java/generic_utility/BaseClass.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -17,24 +18,28 @@ public class BaseClass {
 	public WebDriver driver = null;
 	public static WebDriver sdriver = null;
 	public FileUtility fu = null;
-    public MyWebDriverUtility mwdu = null;
+	public MyWebDriverUtility mwdu = null;
 
-    @BeforeClass
-    public void configBeforeClass() throws IOException{
-        this.fu = new FileUtility();
+	@BeforeClass
+	public void configBeforeClass() throws IOException {
+		this.fu = new FileUtility();
 		String BROWSER = fu.getDataFromPropertiesFile("brow");
-        this.driver = MyWebDriverUtility.browserDriver(BROWSER);
-        sdriver = this.driver;
-        System.out.println("Browser opened");
-    }
+//		this.driver = MyWebDriverUtility.browserDriver(BROWSER);
+		
+		System.setProperty("webdriver.edge.driver", "./resources/msedgedriver.exe");
+		driver = new EdgeDriver();
+		
+		sdriver = this.driver;
+		System.out.println("Browser opened");
+	}
 
-    @BeforeMethod
-    public void login() throws IOException {
+	@BeforeMethod
+	public void login() throws IOException {
 		String URL = fu.getDataFromPropertiesFile("url");
-        String USERNAME = fu.getDataFromPropertiesFile("un");
+		String USERNAME = fu.getDataFromPropertiesFile("un");
 		String PASSWORD = fu.getDataFromPropertiesFile("pwd");
-        this.mwdu = new MyWebDriverUtility(driver);
-        mwdu.getMaximize();
+		this.mwdu = new MyWebDriverUtility(driver);
+		mwdu.getMaximize();
 		mwdu.globalWait();
 
 		driver.get(URL);
@@ -42,21 +47,21 @@ public class BaseClass {
 		LoginPage lp = new LoginPage(driver);
 		lp.getUserNameTF().sendKeys(USERNAME + Keys.TAB + PASSWORD + Keys.ENTER);
 
-        System.out.println("Login is successful");
-    }
+		System.out.println("Login is successful");
+	}
 
-    @AfterMethod
-    public void logout() {
-        Campaigns cp = new Campaigns(driver);
-        WebElement userIcon = cp.getUserIcon();
-        mwdu.waitForElementToBeVisible(userIcon).click();
-        cp.getLogoutbtn().click();
-        System.out.println("Logout is successful");
-    }
-    
-    @AfterClass
-    public void closeBrowser() {
-        driver.quit();
-        System.out.println("Browser closed");
-    }
+	@AfterMethod
+	public void logout() {
+		Campaigns cp = new Campaigns(driver);
+		WebElement userIcon = cp.getUserIcon();
+		mwdu.waitForElementToBeVisible(userIcon).click();
+		cp.getLogoutbtn().click();
+		System.out.println("Logout is successful");
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit();
+		System.out.println("Browser closed");
+	}
 }
